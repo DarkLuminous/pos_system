400b2921939ac9161b544ff810df564ffdc884b1
DarkLuminous <roger.bao101@gmail.com>
2025-09-25 00:54:59 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/Desktop/D_Main.dart
    #	lib/Desktop/Dashboard_Content_D.dart
    #	lib/List_Manager.dart

--- lib/Desktop/Inventory_Content_D.dart
+++ lib/Desktop/Inventory_Content_D.dart
@@ -10,8 +10,10 @@ class InventoryContentD extends StatelessWidget {
   const InventoryContentD({super.key});
 
 
+
   @override
   Widget build(BuildContext context) {
+
     return Container(
       padding: EdgeInsets.all(50),
       child: Column(
@@ -105,32 +107,36 @@ class InventoryContentD extends StatelessWidget {
           Container(
             height: 700,
             width: 1700,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: const Color.fromARGB(255, 193, 225, 153,),
+            ),
             child: DataTable2(
               headingRowColor: WidgetStateColor.resolveWith((states) => Colors.white),
               headingRowHeight: 60,
-              columnSpacing: 0,
+              columnSpacing: 1,
               dividerThickness: 1,
               dataRowHeight: 100,
               minWidth: 1600,
               columns: const [
-                DataColumn2(label: Text("ID", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-                DataColumn2(label: Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-                DataColumn2(label: Text("Product", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-                DataColumn2(label: Text("Stock", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-                DataColumn2(label: Text("Status", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-                DataColumn2(label: Text("Date", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-                DataColumn2(label: Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+                DataColumn2(label: Text("ID", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF1B5E20)))),
+                DataColumn2(label: Text("Product", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF1B5E20)))),
+                DataColumn2(label: Text("Change Type", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF1B5E20)))),
+                DataColumn2(label: Text("Quantity Change", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF1B5E20)))),
+                DataColumn2(label: Text("Remarks", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF1B5E20)))),
+                DataColumn2(label: Text("Date", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF1B5E20)))),
+                DataColumn2(label: Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF1B5E20)))),
               ],
               rows: List.generate(Pro_product.length, (index) {
                 final product = Pro_product[index];
                 return DataRow(cells: [
-                  DataCell(Text(Pro_product[index]['ID']!, style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20))),
-                  DataCell(Container( height: 80, width: 80, margin: EdgeInsets.all(10), color: Colors.white, child: Image.asset(Pro_product[index]['image']!,))),
-                  DataCell(Column( mainAxisAlignment: MainAxisAlignment.center, children: [ Text(Pro_product[index]['name']!, style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20)), Text( NumberFormat.currency( symbol: '₱', decimalDigits: 2, ).format(double.parse(Pro_product[index]['price']!)), style: const TextStyle( fontWeight: FontWeight.w600, fontSize: 15, ), textAlign: TextAlign.center, maxLines: 1, overflow: TextOverflow.ellipsis, ), ], )),
-                  DataCell(Text(Pro_product[index]['stock']!, style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20))),
-                  DataCell(Container(margin: EdgeInsets.all(10), color: Colors.green, child: Text(" In Stock ", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.white, fontSize: 15)))),
+                  DataCell(Row( mainAxisAlignment: MainAxisAlignment.end, children: [Padding(padding: const EdgeInsets.only(right: 70 ), child: Text(Pro_product[index]['ID']!, style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20)),),Container(height: 80, width: 80, margin: EdgeInsets.only(right: 50), color: Colors.white, child: Image.asset(Pro_product[index]['image']!,))],)),
+                  DataCell(Column( mainAxisAlignment: MainAxisAlignment.center, children: [ Text(Pro_product[index]['name']!, style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20)),])),
+                  DataCell(Column( mainAxisAlignment: MainAxisAlignment.center, children: [ Text("Change me!", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15)),])),
+                  DataCell(Text(Pro_product[index]['stock']!, style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15))),
+                  DataCell(Text(" In Stock ", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15))),
                   DataCell(Text(DateFormat("yyyy-MM-dd").format(DateTime.now()))),
-                  DataCell( Row( children: [ ElevatedButton( style: ElevatedButton.styleFrom( backgroundColor: Colors.white, shape: RoundedRectangleBorder( borderRadius: BorderRadius .zero,), ), onPressed: () {}, child: Text("Edit", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15)), ), ElevatedButton( style: ElevatedButton.styleFrom( backgroundColor: Colors.green, shape: RoundedRectangleBorder( borderRadius: BorderRadius .zero, ), ), onPressed: () {}, child: Text("Restock", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.white, fontSize: 15)),
+                  DataCell( Row( children: [ ElevatedButton( style: ElevatedButton.styleFrom( backgroundColor: Colors.white, shape: RoundedRectangleBorder( borderRadius: BorderRadius .zero,), ), onPressed: () {}, child: Text("Edit", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15)), ), ElevatedButton( style: ElevatedButton.styleFrom( backgroundColor: Colors.red, shape: RoundedRectangleBorder( borderRadius: BorderRadius .zero, ), ), onPressed: () {}, child: Text("Delete", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.white, fontSize: 15)),
                 )
                 ]
                 )
